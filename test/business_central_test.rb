7624f3f5ceab6f562eee2571d80b99471ebaf16b
Phil Muldoon <phillip.muldoon@dentally.co>
2021-08-03 09:55:17 +0100

    Fix up test

--- test/business_central_test.rb
+++ test/business_central_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require_relative '../../business-central/lib/business_central/version'
 
 class BusinessCentralTest < Minitest::Test
   def test_version_number_exists
